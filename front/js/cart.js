cd2237133726c539617cf17103495b8bcdebc784
dimitri-lev <dimitri.lev@protonmail.com>
2022-04-29 01:21:13 +0200

    update delete button + formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,12 @@
 let produitLocalStorage = JSON.parse(localStorage.getItem('produit'));
 console.log(produitLocalStorage);
+let total = 0;
+let number = 0;
 
 // pour tout les produits qui se trouvent dans le local storage je crée le html et j'ajoute les informations des produits
 if (produitLocalStorage!=null) {
     for (let product of produitLocalStorage) {
-        /* let img = localStorage.getItem('img')
-        let nom = localStorage.getItem('name')
-        let price = localStorage.getItem('price')
-        let id = localStorage.getItem('id')
-        let color = localStorage.getItem('color')
-        let quantity = localStorage.getItem('quantity') */
-    
+
         let cartItem = document.querySelector('#cart__items')
     
         let article = document.createElement('article')
@@ -78,5 +74,130 @@ if (produitLocalStorage!=null) {
         deleteItem.className = 'deleteItem'
         deleteItem.innerText = 'Supprimer'
         itemContentSettingDelete.appendChild(deleteItem)
+
+        let totalQuantity = document.querySelector('#totalQuantity');
+        totalQuantity.innerText = total+=product.quantity;
+
+        let totalPrice = document.querySelector('#totalPrice');
+        totalPrice.innerText = number += product.price * product.quantity;
     }
 }
+
+let deleteButton = document.querySelectorAll('.deleteItem');
+console.log(deleteButton);
+
+console.log(produitLocalStorage.id);
+
+for (let i = 0; i < deleteButton.length; i++){
+    deleteButton[i].addEventListener('click', function(){
+        let supprimerProduit = produitLocalStorage[i].id;
+        console.log(supprimerProduit);
+        produitLocalStorage = produitLocalStorage.filter(p => p.id != supprimerProduit);
+        console.log(produitLocalStorage);
+        localStorage.setItem('produit', JSON.stringify(produitLocalStorage));
+        /* alert('Ce produit a été supprimé du panier'); */
+        window.location.href ="../html/cart.html";
+    })
+}
+
+let firstName = document.querySelector('#firstName');
+
+    firstName.addEventListener('change', function(){
+        validFirstName(this);
+    });
+
+    const validFirstName = (inputFirstName) => {
+        let firstNameRegExp = new RegExp ("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+
+        let testFirstName = firstNameRegExp.test(inputFirstName.value);
+
+        console.log(testFirstName);
+
+        if (testFirstName == false) {
+            document.querySelector('#firstNameErrorMsg').innerText = 'Prénom invalide';
+        } else {
+            document.querySelector('#firstNameErrorMsg').innerText = 'Prénom valide';
+        };
+    };
+
+let lastName = document.querySelector('#lastName');
+
+    lastName.addEventListener('change', function(){
+        validLastName(this);
+    });
+
+    const validLastName = (inputLastName) => {
+        let lastNameRegExp = new RegExp ("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+
+        let testLastName = lastNameRegExp.test(inputLastName.value);
+
+        console.log(testLastName);
+
+        if (testLastName == false) {
+            document.querySelector('#lastNameErrorMsg').innerText = 'Nom invalide';
+        } else {
+            document.querySelector('#lastNameErrorMsg').innerText = 'Nom valide';
+        };
+    };
+
+let address = document.querySelector('#address');
+
+    address.addEventListener('change', function(){
+        validAddress(this);
+    });
+
+    const validAddress = (inputAddress) => {
+        let addressRegExp = new RegExp ("^[A-za-z0-9]{5,50}$");
+
+        let testAddress = addressRegExp.test(inputAddress.value);
+
+        console.log(testAddress);
+
+        if (testAddress == false) {
+            document.querySelector('#addressErrorMsg').innerText = 'Adresse invalide';
+        } else {
+            document.querySelector('#addressErrorMsg').innerText = 'Adresse valide';
+        };
+    };
+
+let city = document.querySelector('#city');
+
+    city.addEventListener('change', function(){
+        validCity(this);
+    });
+
+    const validCity = (inputCity) => {
+        let cityRegExp = new RegExp ("^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$");
+
+        let testCity = cityRegExp.test(inputCity.value);
+
+        console.log(testCity);
+
+        if (testCity == false) {
+            document.querySelector('#cityErrorMsg').innerText = 'Ville invalide';
+        } else {
+            document.querySelector('#cityErrorMsg').innerText = 'Ville valide';
+        };
+    };
+
+let email = document.querySelector('#email');
+
+    email.addEventListener('change', function(){
+        validEmail(this);
+    });
+
+    const validEmail = (inputEmail) => {
+        let emailRegExp = new RegExp ("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$");
+
+        let testEmail = emailRegExp.test(inputEmail.value);
+
+        console.log(testEmail);
+
+        if (testEmail == false) {
+            document.querySelector('#emailErrorMsg').innerText = 'Email invalide';
+        } else {
+            document.querySelector('#emailErrorMsg').innerText = 'Email valide';
+        };
+    };
+
+let order = document.querySelector('#order');
